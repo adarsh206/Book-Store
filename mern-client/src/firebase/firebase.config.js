0bfc3a2ab06643dfab159f83eddaadcf5095c998
Adarsh Kumar <adarsh2061998@gmail.com>
2024-01-10 15:06:25 +0530

    Update firebase.config.js

--- mern-client/src/firebase/firebase.config.js
+++ mern-client/src/firebase/firebase.config.js
@@ -5,12 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAV3_d4FbuMeSM2GgcjBpwW9dn41lD7jvE",
-  authDomain: "book-store-c5b64.firebaseapp.com",
-  projectId: "book-store-c5b64",
-  storageBucket: "book-store-c5b64.appspot.com",
-  messagingSenderId: "264042536930",
-  appId: "1:264042536930:web:06a2780f1ef6b9d6c4e3bb"
+  YOUR CONFIGURATION HERE
 };
 
 // Initialize Firebase
